dd0643c2fa88d165874054c6d24f7c8b296d27e4
finnatinform <finn.zentgraf@inform-software.com>
2018-03-27 20:41:59 +0200

    Added gulpfile for gulp configuration

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+let gulp = require('gulp');
+
+gulp.task('build', build);
+
+/**
+ * Builds the software
+ * @param {*} _Done 
+ */
+function build( _Done ) {
+  // place code for your default task here
+  _Done();
+}

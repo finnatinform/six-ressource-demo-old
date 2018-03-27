24898e74505905ca0fe385c3b4d0d60995ae2ff0
finnatinform <finn.zentgraf@inform-software.com>
2018-03-27 21:59:07 +0200

    added class for modules

---
+++ src/internal/module.ts
@@ -0,0 +1,13 @@
+export default class Module{
+    private __Path : string ;
+    public Component : React.ComponentClass ;
+
+    public get Path() : string {
+        return '/' + this.__Path ;
+    }
+
+    constructor( _Path : string , _Component : React.ComponentClass ){        
+        this.__Path = _Path ;
+        this.Component = _Component ;
+    }
+}

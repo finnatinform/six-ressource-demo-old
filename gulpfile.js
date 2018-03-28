0c332e5c5784f24b0e576a1df7b9f88ae06059ee
Finn Zentgraf <finn.zentgraf@inform-software.com>
2018-03-28 14:21:38 +0200

    edited gulp for chaining tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ let gulp = require('gulp');
 let gulpCopy = require('gulp-copy');
 let webpack = require('webpack-stream');
 let del = require('del');
+let cordova = require('cordova-lib').cordova ;
+
 
 let webpackClientConfiguration = require('./webpack.client.config');
 let webpackServerConfiguration = require('./webpack.server.config');
@@ -13,23 +15,44 @@ gulp.task('buildServer', buildServer);
 /**
  * Builds the software
  */
-function defaultTask() {
-    buildClient();
-    buildServer();
-
-    deployDesktop();
+function defaultTask( _Done ) {
+    buildMobile( _Done );
 }
 
 function buildServer(){
     return gulp.src('./src')
-        .pipe(webpack( webpackServerConfiguration ))
-        .pipe(gulp.dest('dist'))
+        .pipe(webpack( webpackServerConfiguration )) ;
 }
 
+// quasi prebuild
 function buildClient(){
-    return gulp.src('./src')
+    return new Promise( function(_Resolve , _Reject) {
+        gulp.src('./src')
         .pipe(webpack( webpackClientConfiguration ))
-        .pipe(gulp.dest('dist'))
+        .on('error',_Reject)
+        .pipe(gulp.dest('./dist'))
+        .on('end',_Resolve)
+    }) ;
+}
+
+function prepareMobile(){
+    return new Promise( function(_Resolve , _Reject) {
+        gulp.src( ['./views/mobile.html', './dist/application.js' ])
+        .pipe( gulpCopy( './www', { prefix : 1 } ) )
+        .on('error',_Reject)
+        .on('end',_Resolve)
+    } );
+}
+
+function buildMobile( _Done ){
+    buildClient()
+        .then(prepareMobile)
+        // .then(deployMobile) 
+        .then(_Done);
+}
+
+function deployMobile(){
+    // TODO
 }
 
 function deployDesktop(){

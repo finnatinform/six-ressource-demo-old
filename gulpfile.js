ddd0cb47bcb9317684344d9ca3710cdd4f4127cd
Finn Zentgraf <finn.zentgraf@inform-software.com>
2018-03-28 11:31:44 +0200

    splitted up client and server build

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 let gulp = require('gulp');
 let webpack = require('webpack-stream');
-let webpackConfiguration = require('./webpack.config');
+let webpackClientConfiguration = require('./webpack.client.config');
+let webpackServerConfiguration = require('./webpack.server.config');
 
 gulp.task('default', defaultTask);
+gulp.task('buildClient', buildClient);
+gulp.task('buildServer', buildServer);
 
 /**
  * Builds the software
  */
 function defaultTask() {
+    buildClient();
+    buildServer();
+}
+
+function buildServer(){
     return gulp.src('./src')
-        .pipe(webpack( webpackConfiguration ))
+        .pipe(webpack( webpackServerConfiguration ))
+        .pipe(gulp.dest('dist'))
+}
+
+function buildClient(){
+    return gulp.src('./src')
+        .pipe(webpack( webpackClientConfiguration ))
         .pipe(gulp.dest('dist'))
 }

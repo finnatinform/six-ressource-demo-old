bd8a23922f489d779f8e5bcbc3ea2decb4e544d2
Finn Zentgraf <finn.zentgraf@inform-software.com>
2018-03-28 20:36:18 +0200

    added components for productionslevels

---
+++ src/components/game/production-level.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export interface IProductionLevelProps {  }
+export interface IProductionLevelState {  }
+
+export class ProductionLevel extends React.Component<IProductionLevelProps, IProductionLevelState> {
+    render() {
+        return <h1>Game</h1>;
+    }
+}
---
+++ src/components/game/production-wrapper.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { ProductionLevel } from './production-level';
+
+export interface IProductionWrapperProps {  }
+export interface IProductionWrapperState {  }
+
+export class ProductionWrapper extends React.Component<IProductionWrapperProps, IProductionWrapperState> {
+
+    private renderProductionLevel():Array<ProductionLevel>{
+        let HResult : Array<ProductionLevel> = [] ;
+
+        return HResult ;
+    }
+    render() {
+        return(
+            <
+        );
+    }
+}

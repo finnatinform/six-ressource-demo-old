289638b0e6a794bae2f5ce273488a8d71b31f539
finnatinform <finn.zentgraf@inform-software.com>
2018-03-31 22:40:47 +0200

    Defined Game Subviews

---
+++ src/components/game/game-board.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Switch, Route } from 'react-router';
+
+export interface IGameBoardProps {  }
+export interface IGameBoardState {  }
+
+export class GameBoard extends React.Component<IGameBoardProps, IGameBoardState> {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}

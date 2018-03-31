5ff98ef13cb71ed9c8902b910442ebfea812e3f4
finnatinform <finn.zentgraf@inform-software.com>
2018-03-31 22:47:36 +0200

    Added GameBoard Component

--- src/components/game/game-board.tsx
+++ src/components/game/game-board.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router';
+import { ProductionWrapper } from './production-wrapper';
+import { OrderWrapper } from './orders-wrapper';
 
 export interface IGameBoardProps {  }
 export interface IGameBoardState {  }
@@ -8,7 +10,8 @@ export class GameBoard extends React.Component<IGameBoardProps, IGameBoardState>
     render() {
         return (
             <div>
-                
+                <OrderWrapper />
+                <ProductionWrapper />
             </div>
         );
     }

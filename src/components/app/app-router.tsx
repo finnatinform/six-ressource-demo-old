e0a654b100342df0ecb4d7aca439f829903ef837
Finn Zentgraf <finn.zentgraf@inform-software.com>
2018-04-05 17:12:35 +0200

    updated styles (doesnt work probably)

--- src/components/app/app-router.tsx
+++ src/components/app/app-router.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { HashRouter } from 'react-router-dom';
 
-import { AppModuleMatcher } from './app-module-matcher' ;
-import { Route } from 'react-router';
+import { AppModuleMatcher } from './app-module-matcher';
+import { Route, Switch } from 'react-router';
+import { NotFoundView } from '../views/view-not-found';
 
 export interface IAppRouterProps { }
 export interface IAppRouterState { }
@@ -11,7 +12,10 @@ export class AppRouter extends React.Component<IAppRouterProps, IAppRouterState>
     render(): JSX.Element {
         return (
             <HashRouter >
-                <Route path='/:module' component={AppModuleMatcher} />
+                <Switch>
+                    <Route path='/:module' component={AppModuleMatcher} />
+                    <Route component={NotFoundView} />
+                </Switch>
             </HashRouter>
         );
     }
